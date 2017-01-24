d4cdf4456e27d6bce63b8b316d901ef5ea02612c
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-01-23 23:55:33 -0500

    added the schema in python for requests and items

--- inventoryProject/settings/default.py
+++ inventoryProject/settings/default.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'items.apps.ItemsConfig',
+    'requests.apps.RequestsConfig'
 ]
 
 MIDDLEWARE = [
---
+++ requests/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Request(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='owner', on_delete=models.CASCADE)
+    status = models.CharField(max_length=16, choices=[
+        ('outstanding', 'outstanding'), ('approved', 'approved'),
+        ('cancelled', 'cancelled'), ('denied', 'denied')],default='outstanding')
+    item = models.ForeignKey('items.Item', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    reason = models.TextField(null=True)
+    timestamp = models.TimeField(auto_now=True)
+    admin_timestamp = models.TimeField(null=True)
+    system_comment = models.TextField(null=True)
+    admin_comment = models.TextField(null=True)
+    admin = models.ForeignKey('auth.User', related_name='admin', on_delete=models.CASCADE)
+
+
+
+
+
