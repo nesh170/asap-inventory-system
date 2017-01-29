c78bce1f9ec0a0be2612b5743beca155835449fc
ankitkayastha <ankit.kayastha@duke.edu>
2017-01-29 16:08:04 -0500

    additional functionality

--- requests/urls.py
+++ requests/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^requests/$', views.RequestList.as_view()),
     url(r'^requests/(?P<pk>[0-9]+)/$', views.ViewDetailedRequest.as_view()),
     url(r'^userRequests', views.RequestListUser.as_view()),
+    url(r'^createRequest', views.CreateRequest.as_view()),
+    url(r'^modifyRequest/(?P<pk>[0-9]+)/$', views.ModifyRequest.as_view()),
 ]
