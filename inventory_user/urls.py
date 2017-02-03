3bd46f89ddd299e3a1bf7dc24a895a8acd0a6f43
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-02 21:26:53 -0500

    added a current user api (#56)

--- inventory_user/urls.py
+++ inventory_user/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from inventory_user.views.user_view import InventoryUserList
+from inventory_user.views.user_view import InventoryUserList, InventoryCurrentUser
 from items.views.item_view import ItemList, ItemDetail
 from items.views.tag_view import TagDeletion, TagList
 
 urlpatterns = [
     url(r'^$', InventoryUserList.as_view(), name='user-list'),
+    url(r'^current/$', InventoryCurrentUser.as_view(), name='user-current'),
 ]
--- inventory_user/views/user_view.py
+++ inventory_user/views/user_view.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from inventory_user.serializers.user_serializer import UserSerializer
 
@@ -10,3 +11,14 @@ class InventoryUserList(generics.ListCreateAPIView):
     permission_classes = [IsAdminUser, TokenHasReadWriteScope]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class InventoryCurrentUser(generics.RetrieveAPIView):
+    permission_classes = [IsAdminUser, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
