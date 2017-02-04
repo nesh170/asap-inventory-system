a55d96573b2cd2695067db467a96a7fd72e28f78
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-03 22:21:44 -0500

    added the distinct to tag

--- items/urls.py
+++ items/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
 from items.views.item_view import ItemList, ItemDetail
-from items.views.tag_view import TagDeletion, TagList
+from items.views.tag_view import TagDeletion, TagList, UniqueTagList
 
 urlpatterns = [
     url(r'^$', ItemList.as_view(), name='item-list'),
     url(r'^(?P<pk>[0-9]+)$', ItemDetail.as_view(), name='item-detail'),
     url(r'^tag/$', TagList.as_view(), name='tag-list'),
     url(r'^tag/(?P<pk>[0-9]+)$', TagDeletion.as_view(), name='tag-deletion'),
+    url(r'^tag/unique/$', UniqueTagList.as_view(), name='unique-tag-list'),
 ]
