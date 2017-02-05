5dce36c26434b74d07e2c113d19c92a598e1e6e5
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-05 14:41:52 -0500

    fixed permissions

--- inventory_requests/views/DisburseView.py
+++ inventory_requests/views/DisburseView.py
@@ -1,6 +1,6 @@
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
@@ -9,8 +9,8 @@ from items.models import Item
 from inventory_logger.utility.logger import LoggerUtility
 from inventory_logger.action_enum import ActionEnum
 @api_view(['POST'])
+@permission_classes((TokenHasReadWriteScope, IsAdminUser))
 def DisburseDirectly(request):
-    permission_classes = [TokenHasReadWriteScope, IsAdminUser]
     serializer = DisburseSerializer(data=request.data)
     if serializer.is_valid():
         item = Item.objects.get(pk=serializer.data.get('item_id'))
