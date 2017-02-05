12a179b04d70e0cd086031433c84332d5e524389
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-05 00:03:33 -0500

    updated permissions

--- inventory_user/views/user_view.py
+++ inventory_user/views/user_view.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
 from inventory_user.serializers.user_serializer import UserSerializer
@@ -14,7 +14,7 @@ class InventoryUserList(generics.ListCreateAPIView):
 
 
 class InventoryCurrentUser(generics.RetrieveAPIView):
-    permission_classes = [IsAdminUser, TokenHasReadWriteScope]
+    permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
