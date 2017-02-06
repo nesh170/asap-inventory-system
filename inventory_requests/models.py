7738314da170fd2b6dda4c1386d7bcf79aca05d6
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-05 21:42:44 -0500

    added the receiver

--- inventory_requests/models.py
+++ inventory_requests/models.py
@@ -1,4 +1,11 @@
+import datetime
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+from inventory_logger.action_enum import ActionEnum
+from inventory_logger.utility.logger import LoggerUtility
+from items.models import Item
 
 
 class Request(models.Model):
@@ -6,7 +13,7 @@ class Request(models.Model):
     status = models.CharField(max_length=16, choices=[
         ('outstanding', 'outstanding'), ('approved', 'approved'),
         ('cancelled', 'cancelled'), ('denied', 'denied')], default='outstanding')
-    item = models.ForeignKey('items.Item', on_delete=models.CASCADE)
+    item = models.ForeignKey('items.Item', null=True, on_delete=models.SET_NULL)
     quantity = models.PositiveIntegerField(default=0)
     reason = models.TextField()
     timestamp = models.DateTimeField(auto_now=True)
@@ -17,6 +24,20 @@ class Request(models.Model):
     def __str__(self):
         request_string = "Request has the following parameters: owner: {owner}, status: {status}, item: {item}, quantity: {quantity}, reason: {reason}, timestamp: {timestamp}, admin_comment: {admin_comment}, admin: {admin}".format
         return request_string(owner=self.owner, status=self.status, item=self.item, quantity=self.quantity, reason=self.reason, timestamp=self.timestamp, admin_timestamp=self.admin_timestamp, admin_comment=self.admin_comment, admin=self.admin)
+
+    @receiver(pre_delete, sender=Item, dispatch_uid='item_got_deleted')
+    def item_got_deleted(sender, instance, using, **kwargs):
+        print('test the cases')
+        requests = Request.objects.filter(item=instance)
+        for request in requests:
+            request.admin_comment = instance.name + "has been deleted by admin"
+            request.admin_timestamp = datetime.datetime.now()
+            if request.status == 'outstanding':
+                request.status = 'denied'
+                LoggerUtility.log_as_system(ActionEnum.REQUEST_DENIED, 'Request:' + request.id + ' denied due to item'
+                                            + instance.name + ' deletion')
+            request.save()
+
 
 
 
