5b244fc5d42a20412a2099cdb60f75826173b2f3
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-06 21:54:30 -0500

    added view for delete user

--- inventory_user/urls.py
+++ inventory_user/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from inventory_user.views.user_view import InventoryUserList, InventoryCurrentUser
+from inventory_user.views.user_view import InventoryUserList, InventoryCurrentUser, InventoryUser
 from items.views.item_view import ItemList, ItemDetail
 from items.views.tag_view import TagDeletion, TagList
 
 urlpatterns = [
     url(r'^$', InventoryUserList.as_view(), name='user-list'),
     url(r'^current/$', InventoryCurrentUser.as_view(), name='user-current'),
+    url(r'^(?P<pk>[0-9]+)$', InventoryUser.as_view(), name='user-detail'),
 ]
