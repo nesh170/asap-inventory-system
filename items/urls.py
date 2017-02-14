ef554e724d353577655a6266022c843ab2a4fe42
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-14 16:11:57 -0500

    custom fields are implemented, trigger test cases

--- items/urls.py
+++ items/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
-from items.views.field_view import FieldList, FieldDeletion
+from items.views.field_view import FieldList, FieldDeletion, IntFieldUpdate, FloatFieldUpdate, ShortTextFieldUpdate, \
+    LongTextFieldUpdate
 from items.views.item_view import ItemList, ItemDetail, UniqueItemList
 from items.views.tag_view import TagDeletion, TagList, UniqueTagList
 
@@ -13,4 +14,8 @@ urlpatterns = [
     url(r'^tag/unique/$', UniqueTagList.as_view(), name='unique-tag-list'),
     url(r'^field/$', FieldList.as_view(), name='field-list'),
     url(r'^field/(?P<pk>[0-9]+)$', FieldDeletion.as_view(), name='field-deletion'),
+    url(r'^field/int/(?P<pk>[0-9]+)$', IntFieldUpdate.as_view(), name='int-field-update'),
+    url(r'^field/float/(?P<pk>[0-9]+)$', FloatFieldUpdate.as_view(), name='float-field-update'),
+    url(r'^field/shorttext/(?P<pk>[0-9]+)$', ShortTextFieldUpdate.as_view(), name='short-text-field-update'),
+    url(r'^field/longtext/(?P<pk>[0-9]+)$', LongTextFieldUpdate.as_view(), name='long-text-field-update'),
 ]
