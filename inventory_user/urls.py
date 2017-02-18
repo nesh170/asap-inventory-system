07efaa7f22ac84035fb5214d0ca60038dcc332c0
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-18 17:20:54 -0500

    Added the get token method

--- inventory_user/urls.py
+++ inventory_user/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
-from inventory_user.views.user_view import InventoryUserList, InventoryCurrentUser, InventoryUser, LargeUserList
+from inventory_user.views.user_view import InventoryUserList, InventoryCurrentUser, InventoryUser, LargeUserList, \
+    get_duke_access_token
 
 urlpatterns = [
     url(r'^$', InventoryUserList.as_view(), name='user-list'),
     url(r'^large/$', LargeUserList.as_view(), name='large-user-list'),
     url(r'^current/$', InventoryCurrentUser.as_view(), name='user-current'),
     url(r'^(?P<pk>[0-9]+)$', InventoryUser.as_view(), name='user-detail'),
+    url(r'^auth/duke$', get_duke_access_token, name='auth-duke')
 ]
--- inventory_user/views/user_view.py
+++ inventory_user/views/user_view.py
@@ -1,13 +1,19 @@
+import requests
 from django.contrib.auth.models import User
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 from rest_framework import filters
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 from inventoryProject.permissions import IsAdminOrReadOnly
 from inventory_user.serializers.user_serializer import UserSerializer, LargeUserSerializer
 from items.custom_pagination import LargeResultsSetPagination
+
+from django.conf import settings
+import json
 
 
 class InventoryUserList(generics.ListCreateAPIView):
@@ -39,5 +45,25 @@ class LargeUserList(generics.ListAPIView):
     pagination_class = LargeResultsSetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('username', )
+
+
+@api_view(['POST'])
+def get_duke_access_token(request):
+    if request.method == 'POST':
+        code = request.data.get('code')
+        redirect_uri = request.data.get('redirect_uri')
+        if code is None or redirect_uri is None:
+            raise ParseError(detail='Require code and redirect_uri in body')
+        data = {
+            'grant_type': 'authorization_code',
+            'client_id': settings.SOCIAL_AUTH_DUKE_KEY,
+            'client_secret': settings.SOCIAL_AUTH_DUKE_SECRET,
+            'code': code,
+            'redirect_uri': redirect_uri
+        }
+        response = requests.post(url='https://oauth.oit.duke.edu/oauth/token', data=data)
+        return Response(data=response.json())
+
+
 
 
