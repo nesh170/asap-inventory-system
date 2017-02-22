b6c014b1499d22e513f1e5990d5b09f5c9c84afa
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-21 23:32:27 -0500

    registered models in admin site

--- inventory_shopping_cart/admin.py
+++ inventory_shopping_cart/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from inventory_shopping_cart.models import ShoppingCart
 
 # Register your models here.
+admin.site.register(ShoppingCart)
--- inventory_shopping_cart_request/admin.py
+++ inventory_shopping_cart_request/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from inventory_shopping_cart_request.models import RequestTable
 # Register your models here.
+admin.site.register(RequestTable)
