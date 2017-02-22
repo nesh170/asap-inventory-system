27e77bd121bc695402d6db9274b72377e4019233
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-21 23:51:41 -0500

    deleted inventory requests

--- inventory_requests/serializers/CancelSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from inventory_requests.models import Request
-
-
-class CancelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Request
-        fields = ('id', 'reason')
--- inventory_requests/serializers/StatusSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from inventory_requests.models import Request
-
-
-class StatusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Request
-        fields = ('id', 'admin_comment', 'admin_timestamp', 'admin')
