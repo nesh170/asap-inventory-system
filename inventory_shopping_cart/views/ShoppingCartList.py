9bd046fcfe890e8ef5233ea275ecc3578ec6ce4a
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-22 23:46:47 -0500

    added filter and search

--- inventory_shopping_cart/views/ShoppingCartList.py
+++ inventory_shopping_cart/views/ShoppingCartList.py
@@ -3,11 +3,14 @@ from rest_framework.permissions import IsAuthenticated
 
 from inventory_shopping_cart.models import ShoppingCart
 from inventory_shopping_cart.serializers.ShoppingCartSerializer import ShoppingCartSerializer
-
+from rest_framework import filters
 
 class ShoppingCartList(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = ShoppingCartSerializer
+    filter_backends = (filters.SearchFilter, filters.DjangoFilterBackend)
+    filter_fields = ('owner__username', 'status')
+    search_fields = ('owner__username', 'reason')
     def get_queryset(self):
         user = self.request.user
         return ShoppingCart.objects.exclude(status="cancelled") if user.is_staff \
