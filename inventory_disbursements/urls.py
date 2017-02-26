529ef4db013562c43e8ad02aaa0f280c559412f9
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-26 02:40:12 -0500

    Added change model (#129)
    * changed quantity requested to quantity
    
    * added the change model

--- inventory_disbursements/urls.py
+++ inventory_disbursements/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 
-from inventory_disbursements.views.disbursement_view import CartList, DisbursementCreation, DisbursementDeletion, \
-    ActiveCart, CartSubmission
+from inventory_disbursements.views.disbursement_view import CartList, DisbursementCreation, ActiveCart, \
+    CartSubmission, DisbursementUpdateDeletion
 
 urlpatterns = [
     url(r'^$', CartList.as_view(), name='cart-list'),
     url(r'^(?P<pk>[0-9]+)$', CartSubmission.as_view(), name='cart-submission'),
     url(r'^active/$', ActiveCart.as_view(), name='active-disbursement-cart'),
-    url(r'^disbursements/(?P<pk>[0-9]+)$', DisbursementDeletion.as_view(), name='disbursement-deletion'),
+    url(r'^disbursements/(?P<pk>[0-9]+)$', DisbursementUpdateDeletion.as_view(), name='disbursement-edits'),
     url(r'disbursements/create/', DisbursementCreation.as_view(), name='disbursement-creation')
 ]
