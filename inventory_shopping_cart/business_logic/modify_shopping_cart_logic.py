215d56971859dc33c2f75372b0312d71743fd398
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-02-26 02:03:23 -0500

    changed quantity requested to quantity

--- inventory_shopping_cart/business_logic/modify_shopping_cart_logic.py
+++ inventory_shopping_cart/business_logic/modify_shopping_cart_logic.py
@@ -10,14 +10,14 @@ def get_item(pk):
 def can_approve_deny_cancel_shopping_cart(shopping_cart_to_modify, modificationType):
     for request in shopping_cart_to_modify.requests.all():
         item = get_item(request.item.id)
-        if (item.quantity - request.quantity_requested < 0 and modificationType == "approved"):
+        if (item.quantity - request.quantity < 0 and modificationType == "approved"):
             return False
     return shopping_cart_to_modify.status == "outstanding"
 
 def approve_shopping_cart(shopping_cart_to_modify):
     for request in shopping_cart_to_modify.requests.all():
         item = get_item(request.item.id)  # get item object based on item ID
-        item.quantity = item.quantity - request.quantity_requested
+        item.quantity = item.quantity - request.quantity
         item.save()
 
 def cancel_shopping_cart(shopping_cart_to_modify):
