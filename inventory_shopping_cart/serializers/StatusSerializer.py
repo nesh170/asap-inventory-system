569fe6e3a936545b02dbf687a1d0b8f5c77f8226
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-28 00:24:12 -0500

    changes why u throw 500

--- inventory_shopping_cart/serializers/StatusSerializer.py
+++ inventory_shopping_cart/serializers/StatusSerializer.py
@@ -3,6 +3,9 @@ from inventory_shopping_cart.models import ShoppingCart
 
 
 class StatusSerializer(serializers.ModelSerializer):
+    admin = serializers.SlugRelatedField(read_only=True, many=False, slug_field='username')
+
+
     class Meta:
         model = ShoppingCart
         fields = ('id', 'admin_comment', 'admin_timestamp', 'admin')
