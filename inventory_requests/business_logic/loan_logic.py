00bdc568afd1a9e111c6528375043231b9d19ad1
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-03-27 00:49:08 -0400

    loan logic is fixed to now append the loans (#209)

--- inventory_requests/business_logic/loan_logic.py
+++ inventory_requests/business_logic/loan_logic.py
@@ -8,12 +8,15 @@ def return_loan_logic(loan, quantity=None):
         if quantity is None or loan.returned_quantity + quantity == loan.quantity:
             loan.returned_timestamp = datetime.now()
             quantity = loan.quantity
+            new_returned_quantity = quantity
+        else:
+            new_returned_quantity = loan.returned_quantity + quantity
         if loan.returned_quantity + quantity > loan.quantity and quantity != loan.quantity:
             return False
         item = Item.objects.get(pk=loan.item.id)
         item.quantity = item.quantity + quantity
         item.save()
-        loan.returned_quantity = quantity
+        loan.returned_quantity = new_returned_quantity
         loan.save()
         return True
     return False
