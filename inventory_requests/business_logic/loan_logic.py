cc2d464fde8a8f80ba91e0bfff44b874f5dcd5ca
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-04-17 01:29:31 -0400

    added the return loan logic for backfill

--- inventory_requests/business_logic/loan_logic.py
+++ inventory_requests/business_logic/loan_logic.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from inventory_requests.business_logic.modify_request_cart_logic import get_backfill_quantity
 from items.models.item_models import Item
 
 
@@ -15,14 +16,19 @@ def match_loan_asset(loan, asset):
 
 def return_loan_logic(loan, quantity=None, asset=None):
     if loan.returned_timestamp is None and loan.loaned_timestamp is not None \
-            and loan.returned_quantity < loan.quantity and match_loan_asset(loan,asset):
-        if quantity is None or loan.returned_quantity + quantity == loan.quantity:
+            and loan.returned_quantity < loan.quantity and match_loan_asset(loan, asset):
+        max_quantity_to_return = (loan.quantity - get_backfill_quantity(loan.backfill_loan, backfill_transit=True))
+        if quantity is None or (loan.returned_quantity + quantity) == max_quantity_to_return:
+            # this case means return everything :/
             loan.returned_timestamp = datetime.now()
-            quantity = loan.quantity
+            quantity = max_quantity_to_return # condition A
             new_returned_quantity = quantity
         else:
+            # this is for partial return
             new_returned_quantity = loan.returned_quantity + quantity
-        if loan.returned_quantity + quantity > loan.quantity and quantity != loan.quantity:
+        if loan.returned_quantity + quantity > max_quantity_to_return and quantity != max_quantity_to_return:
+            # test whether the returned quantity + the quantity cannot be greater than the max_quantity to return
+            # the first case will occur when condition A occurs, hence to prevent fail the second case needs to occur
             return False
         item = Item.objects.get(pk=loan.item.id)
         item.quantity = item.quantity + quantity
