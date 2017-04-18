6fd8bd6a492f5dc25c7c5cf5d0431aff80fc0d7e
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2017-04-18 04:59:23 -0400

    added the s in asset_ids

--- inventory_requests/views/BackfillView.py
+++ inventory_requests/views/BackfillView.py
@@ -307,9 +307,9 @@ class FailBackfillRequest(APIView):
 def validate_assets(data, backfill_request_to_satisfy, item):
     if item.is_asset:
         if data.get('asset_ids'):
-            json_asset_ids = data.get('asset_id')
+            json_asset_ids = data.get('asset_ids')
             if not isinstance(json_asset_ids, list):
-                raise ParseError(detail='Asset ids must be in list')
+                raise ParseError(detail='Asset ids must be in list, it is in {type}'.format(type=type(json_asset_ids)))
             assets = item.assets.filter(pk__in=[id.get('asset_id') for id in json_asset_ids],
                                         loan=backfill_request_to_satisfy.loan)
             if not (backfill_request_to_satisfy.quantity == assets.count()):
